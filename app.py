508711799be5c022a0aa1474af3e9186a308a4ea
zhijianli9999 <zhijianli9999@gmail.com>
2022-01-31 14:56:29 -0800

    basic site structure

--- app.py
+++ app.py
@@ -25,94 +25,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-# def main():
-#     return render_template("main.html")
-# comment out the below to focus on just the fundamentals
-# after running
-# $ export FLASK_ENV=development; flask run
-# site will be available at
-# http://localhost:5000
 def main():
     return render_template('main_better.html')
 
-# Show url matching
+
+@app.route('/view/')
+def view():
+    return render_template('view.html')
 
 
-@app.route('/hello/')
-def hello():
-    return render_template('hello.html')
-
-
-@app.route('/hello/<name>/')
-def hello_name(name):
-    return render_template('hello.html', name=name)
-
-# Page with form
-
-
-@app.route('/ask/', methods=['POST', 'GET'])
-def ask():
-    if request.method == 'GET':
-        return render_template('ask.html')
-    else:
-        try:
-            return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-        except:
-            return render_template('ask.html')
-
-# File uploads and interfacing with complex Python
-# basic version
-
-
-@app.route('/submit-basic/', methods=['POST', 'GET'])
-def submit_basic():
-    if request.method == 'GET':
-        return render_template('submit-basic.html')
-    else:
-        try:
-            return render_template('submit-basic.html', thanks=True)
-        except:
-            return render_template('submit-basic.html', error=True)
-
-# nontrivial version: makes a prediction and shows a viz
-
-
-@app.route('/submit-advanced/', methods=['POST', 'GET'])
+@app.route('/submit/')
 def submit():
-    if request.method == 'GET':
-        return render_template('submit.html')
-    else:
-        try:
-            # retrieve the image
-            img = request.files['image']
-            img = np.loadtxt(img)
+    return render_template('submit.html')
 
-            # reshape into appropriate format for prediction
-            x = img.reshape(1, 64)
 
-            # load up a pre-trained model and get a prediction
-            model = pickle.load(open("mnist-model/model.pkl", 'rb'))
-            d = model.predict(x)[0]
-
-            # plot the image itself
-            fig = Figure(figsize=(3, 3))
-            ax = fig.add_subplot(1, 1, 1,)
-            ax.imshow(img, cmap="binary")
-            ax.axis("off")
-
-            # in order to show the plot on flask, we need to do a few tricks
-            # Convert plot to PNG image
-            # need to:
-            # import io
-            # import base64
-            pngImage = io.BytesIO()
-            FigureCanvas(fig).print_png(pngImage)
-
-            # Encode PNG image to base64 string
-            pngImageB64String = "data:image/png;base64,"
-            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
-
-            # finally we can render the template with the prediction and image
-            return render_template('submit.html', digit=d, image=pngImageB64String)
-        except:
-            return render_template('submit.html', error=True)
+# def get_message_db():
