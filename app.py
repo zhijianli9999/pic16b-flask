c19141412ba10d4604fc6d76cc6038a08824fa7b
zhijianli9999 <zhijianli9999@gmail.com>
2022-01-31 19:45:15 -0800

    enable submissions

--- app.py
+++ app.py
@@ -4,24 +4,13 @@
 
 from flask import Flask, g, render_template, request
 
-import sklearn as sk
-# import matplotlib.pyplot as plt
-import numpy as np
-import pickle
-
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+import sqlite3
 
 import io
 import base64
 
 
-# Create web app, run with flask run
-# (set "FLASK_ENV" variable to "development" first!!!)
-
 app = Flask(__name__)
-
-# Create main page (fancy)
 
 
 @app.route('/')
@@ -34,9 +23,41 @@ def view():
     return render_template('view.html')
 
 
-@app.route('/submit/')
+def get_message_db():
+    if 'message_db' not in g:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+        cursor = g.message_db.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS messages(
+            id int,
+            handle varchar(255),
+            message varchar(255)
+        )""")
+    return g.message_db
+
+
+def insert_message(request):
+    msg = request.form['message']
+    hnd = request.form['handle']
+    db = get_message_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(id) FROM messages")
+    # set id as number of rows of messages
+    id = cursor.fetchone()[0]
+    cmd = """
+    INSERT INTO messages (id, handle, message)
+    VALUES (""" + str(id) + """,' """ + hnd + """','""" + msg + """')
+    """
+    cursor.execute(cmd)
+    db.commit()
+    db.close()
+    return msg, hnd
+
+
+@app.route('/submit/', methods=['POST', 'GET'])
 def submit():
-    return render_template('submit.html')
-
-
-# def get_message_db():
+    if request.method == 'GET':
+        return render_template('submit.html')
+    else:
+        insert_message(request=request)
+        return render_template('submit.html', thanks=True)
